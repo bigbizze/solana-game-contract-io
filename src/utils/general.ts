00b2e8f543caf54cef43bf06be0d4fa9e1412aa4
Charles Anderson <hpcngmoh@gmail.com>
2022-07-14 14:18:22 -0400

    more helpers

--- src/utils/general.ts
+++ src/utils/general.ts
@@ -1,5 +1,17 @@
+import { Result } from "../shared-types";
+
+
 export const isTypeOne = <T1, T2>(val: T1 | T2, cond: boolean): val is T1 => cond;
 
+export const sleep = async (duration: number) => await new Promise(r => setTimeout(r, duration));
+
+export const unwrapResult = <T>(result: Result<T>): T => {
+  if (result instanceof Error) {
+    console.error(result);
+    process.exit(1);
+  }
+  return result;
+};
 
 export const catchWrapper = async <T>(fn: () => Promise<T>) => {
   try {
